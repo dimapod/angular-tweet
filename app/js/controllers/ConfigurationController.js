494158ce2fb60d5e63dd4059ee7f10907ffe047a
dimapod <dimapod@gmail.com>
2013-02-23 19:34:08 +0100

    Move local storage management to service

--- app/js/controllers/ConfigurationController.js
+++ app/js/controllers/ConfigurationController.js
@@ -1,13 +1,9 @@
 'use strict';
 
 twitterClientApp.controller('ConfigurationCtrl',
-    function ConfigurationCtrl(localStorage, $scope, $location) {
+    function ConfigurationCtrl(localStorage, $scope, configuration) {
 
-        if(localStorage['form']) {
-            $scope.params = JSON.parse(localStorage['form']);
-        } else {
-            $scope.params = [];
-        }
+        $scope.params = configuration.params;
 
         $scope.displayForm = false;
 
@@ -15,19 +11,17 @@ twitterClientApp.controller('ConfigurationCtrl',
 
         $scope.toggleTweetInfo = function() {
             $scope.displayForm = !$scope.displayForm;
-        }
+        };
 
         $scope.addHashTag = function() {
             $scope.params.push({
                 type: 'hash',
                 value:$scope.hashTag});
             $scope.hashTag = "";
-            localStorage['form'] = JSON.stringify($scope.params);
-        }
+        };
 
         $scope.removeHashTag = function(index){
             $scope.params.splice(index, 1);
-            localStorage['form'] = JSON.stringify($scope.params);
-        }
+        };
 
     });
